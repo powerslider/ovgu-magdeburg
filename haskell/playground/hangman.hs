733ecc827b4e5d8971c0abf102b1c2c892745e59
Tsvetan Dimitrov <tsvetan.dimitrov23@gmail.com>
2015-06-17 16:48:42 +0200

    Added a hangman game haskell program

---
+++ haskell/playground/hangman.hs
@@ -0,0 +1,28 @@
+hangman :: IO ()
+hangman = do putStrLn "Think of a word:"
+             word <- sgetLine
+             putStrLn "Try to guess it!"
+             guess word
+
+sgetLine :: IO String
+sgetLine = do x <- getChar
+              if x == '\n' then
+                  do putChar x
+                     return []
+              else 
+                  do putChar '-'
+                     xs <- sgetLine
+                     return (x:xs)
+
+guess :: String -> IO ()
+guess word = do putStr "> "
+                xs <- getLine
+                if xs == word then
+                    putStrLn "You got it!"
+                else 
+                    do putStrLn (diff word xs)
+                       guess word
+
+diff :: String -> String -> String
+diff xs ys = 
+        [if elem x ys then x else '-' | x <- xs]
